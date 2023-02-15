915b2507b23f9872942b2e240c49402337c43840
евгений <genypiano@mail.ru>
2023-02-15 23:03:16 +0400

    домашняя работа, семинар 1

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите два чиcла:");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+if (firstNum > secondNum)
+{
+    Console.WriteLine(firstNum + " больше " + secondNum + ", max = " + firstNum);
+}
+else if(firstNum == secondNum)
+{
+    Console.WriteLine(firstNum + " и " + secondNum + " равны " + ", max = " + firstNum);
+}
+else
+{
+    Console.WriteLine(firstNum + " меньше " + secondNum + ", max = " + secondNum);
+}
+Console.WriteLine("Введи в терминал команду dotnet run и попробуйте снова");
+
+
---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите три чиcла:");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+int secondNum = Convert.ToInt32(Console.ReadLine());
+int thirdNum = Convert.ToInt32(Console.ReadLine());
+
+if ((firstNum > secondNum) && (firstNum > thirdNum))
+{
+    Console.WriteLine(firstNum + " больше " + secondNum + " и больше " + thirdNum + ", max = " + firstNum);
+}
+else if ((firstNum == secondNum) && (firstNum > thirdNum))
+{
+    Console.WriteLine(firstNum + " и " + secondNum + " равны " + "и больше " + thirdNum + ", max = " + firstNum);
+
+}
+else if ((secondNum > firstNum) && (secondNum > thirdNum))
+{
+    Console.WriteLine(secondNum + " больше " + firstNum + " и больше " + thirdNum + ", max = " + secondNum);
+}
+else if ((secondNum > firstNum) && (secondNum == thirdNum))
+{
+    Console.WriteLine(secondNum + " и " + thirdNum + " равны " + "и больше " + firstNum + ", max = " + secondNum);
+}
+else if ((firstNum == thirdNum) && (firstNum > secondNum))
+{
+    Console.WriteLine(firstNum + " и " + thirdNum + " равны " + "и больше " + secondNum + ", max = " + firstNum);
+}
+else if ((firstNum == thirdNum) && (firstNum == secondNum))
+{
+    Console.WriteLine(firstNum + " и " + secondNum + " и " + thirdNum + " равны " + ", max = " + firstNum);
+}
+else
+{
+    Console.WriteLine(thirdNum + " больше " + firstNum + " и больше " + secondNum + ", max = " + thirdNum);
+}
+Console.WriteLine("Введи в терминал команду dotnet run и попробуйте снова");
+
